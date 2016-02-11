8e9f6605216083c7e17a73c12af78c61e5bb286d
Alexis Oblet <alexis.oblet@gmail.com>
2016-02-11 22:33:18 +0100

    Add base CameraFreefly Front and left front displacement, rotation. But no straight movement: useless for this project with splines

---
+++ include/view/CameraFreefly.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+
+/**
+ * Freefly camera: free movement Camera. Like spectator FPS game camera.
+ * Looks by default in Z direction.
+ */
+
+class CameraFreefly {
+    glm::mat4 _matrix;          /** Transform matrix obtained by glm::lookAt */
+    glm::vec3 _eye;             /** World camera position */
+    glm::vec3 _up;              /** Up world vector */
+    glm::vec3 _front;           /** Where the camera looks */
+    glm::vec3 _frontLeft;       /** Left vector from front: used for Up*/
+    glm::vec2 _sphericalAngles; /** In radians */
+
+public:
+    //TODO: think about pos and speed splines attachement
+    void moveFront(float speed);
+    void moveLeft(float speed);
+    void rotate(const glm::vec2& angles);
+    void computeDirs();
+    void update(const glm::vec2& angles, float speedFront, float speedLeft);
+    void setEye(const glm::vec3& eye);
+    glm::vec3 getEye() const;
+    const glm::mat4& getViewMatrix() const;
+};
+
---
+++ src/view/CameraFreefly.cpp
@@ -0,0 +1,50 @@
+#include "view/CameraFreefly.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+void CameraFreefly::computeDirs() {
+    // Spherical coordinates
+    _front.x = glm::cos(_sphericalAngles.y) * glm::cos(_sphericalAngles.x);
+    _front.y = glm::sin(_sphericalAngles.y);
+    _front.z = glm::cos(_sphericalAngles.x) * glm::cos(_sphericalAngles.y);
+
+    // Simply compute the left vector of front: rotate of _sphericalAngles.x + PI/2 with y equals to 0
+    _frontLeft.x = glm::sin(_sphericalAngles.x + glm::half_pi<float>());
+    _frontLeft.y = 0;
+    _frontLeft.z = glm::cos(_sphericalAngles.x + glm::half_pi<float>());
+
+    // Cross product to find _up from _front and _frontLeft
+    _up = glm::cross(_front, _frontLeft);
+
+}
+
+const glm::mat4& CameraFreefly::getViewMatrix() const {
+    return _matrix;
+}
+
+void CameraFreefly::moveFront(float speed) {
+    _eye += (_front * speed);
+}
+
+void CameraFreefly::moveLeft(float speed) {
+    _eye += (_frontLeft * speed);
+}
+
+void CameraFreefly::rotate(const glm::vec2& angles) {
+    _sphericalAngles += angles;
+}
+
+void CameraFreefly::update(const glm::vec2 &angles, float speedFront, float speedLeft) {
+    rotate(angles);
+    computeDirs();
+    moveFront(speedFront);
+    moveLeft(speedLeft);
+    _matrix = glm::lookAt(_eye, _eye + _front, _up);
+}
+
+void CameraFreefly::setEye(const glm::vec3 &eye) {
+    _eye = eye;
+}
+
+glm::vec3 CameraFreefly::getEye() const {
+    return _eye;
+}
