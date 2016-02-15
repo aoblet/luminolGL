bdceb4717e97ce003783d4bffbe4ffa4e276c171
sayanel <maximilien.glineur@hotmail.fr>
2016-02-15 18:41:49 +0100

    init gui namespace

---
+++ include/gui/gui.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include "imgui/imgui.h" 
+#include "imgui/imguiRenderGL3.h"
+
+namespace Gui{
+    /**
+    * Freefly camera: free movement Camera. Like spectator FPS game camera.
+    * Looks by default in Z direction.
+    */
+
+    class Gui {
+
+
+    
+
+    }
+}
+
+
---
+++ src/gui/gui.cpp
@@ -0,0 +1,10 @@
+
+#include "gui/gui.hpp"
+
+ 
+namespace Gui{
+	
+
+
+
+}
