8ed9d1e1df12f6dab8ba59519f192e9e995f7fb5
Alexis Oblet <alexis.oblet@gmail.com>
2016-02-16 13:31:17 +0100

    typo

--- src/ui/UserInput.cpp
+++ src/ui/UserInput.cpp
@@ -5,13 +5,13 @@ using namespace UI;
 UserInput::UserInput(float _velocityRotate): velocityRotate(_velocityRotate) { }
 
 void UserInput::update(GLFWwindow *window) {
-    is_PRESSED_Q = glfwGetKey(window, GLFW_KEY_A ) == GLFW_PRESS;
-    is_PRESSED_Z = glfwGetKey(window, GLFW_KEY_W ) == GLFW_PRESS;
-    is_PRESSED_S = glfwGetKey(window, GLFW_KEY_S ) == GLFW_PRESS;
-    is_PRESSED_D = glfwGetKey(window, GLFW_KEY_D ) == GLFW_PRESS;
-    is_PRESSED_SPACE = glfwGetKey(window, GLFW_KEY_SPACE ) == GLFW_PRESS;
-    is_PRESSED_ALT = glfwGetKey(window, GLFW_KEY_LEFT_ALT ) == GLFW_PRESS;
-    velocityMovement = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ? 2.f : 1;
+    is_PRESSED_Q        = glfwGetKey(window, GLFW_KEY_A ) == GLFW_PRESS;
+    is_PRESSED_Z        = glfwGetKey(window, GLFW_KEY_W ) == GLFW_PRESS;
+    is_PRESSED_S        = glfwGetKey(window, GLFW_KEY_S ) == GLFW_PRESS;
+    is_PRESSED_D        = glfwGetKey(window, GLFW_KEY_D ) == GLFW_PRESS;
+    is_PRESSED_SPACE    = glfwGetKey(window, GLFW_KEY_SPACE ) == GLFW_PRESS;
+    is_PRESSED_ALT      = glfwGetKey(window, GLFW_KEY_LEFT_ALT ) == GLFW_PRESS;
+    velocityMovement    = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ? 2.f : 1;
 
     if(glfwGetMouseButton( window, GLFW_MOUSE_BUTTON_MIDDLE) != GLFW_PRESS){
         glfwGetCursorPos(window, &mousePosition.x, &mousePosition.y);
